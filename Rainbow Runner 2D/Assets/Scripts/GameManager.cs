73ca81dd2fa4f4b8b0b8d4a5d265d12e6ef263f9
Tyler Wong <tywong8@gmail.com>
2023-08-10 21:07:04 -0400

    simple jump

---
+++ Rainbow Runner 2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // TODO
+    // Platform speeds
+    // Animators
+    // Obstacle stuff (speed, etc)
+    // Powerup stuff
+    // HUD references
+    // Deal with game starting, losing, restarting
+    // Audio Sources, Game Music, Game Ambience
+
+    // Singleton
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
