3dd9fa3716e573cf81a36428db72adb58d9e689d
Tyler Wong <tywong8@gmail.com>
2023-08-11 18:40:26 -0400

    modifying jump

--- Rainbow Runner 2D/Assets/Scripts/Player2.cs
+++ Rainbow Runner 2D/Assets/Scripts/Player2.cs
@@ -12,15 +12,20 @@ public class Player2 : MonoBehaviour
 
     [SerializeField] float moveSpeed = 40f;
 
-    [SerializeField] bool isHoldingJump = false;
-    [SerializeField] float maxHoldJumpTime = 0.4f;
-    [SerializeField] float holdJumpTimer = 0.0f;
-    [SerializeField] float jumpGrace = 1;
+    private bool pressedJump = false;
+    private bool releasedJump = false;
+    [SerializeField] float gravityScale = 1;
+
+    [SerializeField] float jumpTimer = 0.5f;
+    private bool startTimer = false;
+    private float timer;
 
     private void Awake()
     {
         rb = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
+
+        timer = jumpTimer;
     }
 
     // Start is called before the first frame update
@@ -32,54 +37,68 @@ public class Player2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsGrounded() /*&& Input.GetKeyDown(KeyCode.Space)*/)
-        {       
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.velocity = Vector2.up * jumpVelocity;
-                isHoldingJump = true;
-                holdJumpTimer = 0;
-            }
-             
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            pressedJump = true;
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            isHoldingJump = false;
+            releasedJump = true;
         }
+
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;   
+            if (timer <= 0)
+            {
+                releasedJump = true;
+            }
+        }
+
+
+        /*if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {                
+            rb.velocity = Vector2.up * jumpVelocity;                     
+        }*/
 
         HandleMovement();
     }
 
     private void FixedUpdate()
     {
-        Vector2 pos = transform.position;      
-
-        if (!IsGrounded())
+        if (pressedJump)
         {
-            if (isHoldingJump)
-            {
-                holdJumpTimer += Time.fixedDeltaTime;
-                if (holdJumpTimer >= maxHoldJumpTime)
-                {
-                    isHoldingJump = false;
-                }
-            }
-
-            pos.y += rb.velocity.y * Time.fixedDeltaTime;
-
-            if (!isHoldingJump)
-            {
-                Vector2 velocity = rb.velocity;
-                velocity.y += rb.gravityScale * Time.fixedDeltaTime;
-            }
+            StartJump();
         }
+
+        if (releasedJump)
+        {
+            StopJump();
+        }
+    }
+
+    private void StartJump()
+    {
+        rb.gravityScale = 0;
+        rb.velocity = Vector2.up * jumpVelocity;
+        pressedJump = false;
+        startTimer = true;
+    }
+
+    private void StopJump()
+    {
+        rb.gravityScale = gravityScale;
+        releasedJump = false;
+        timer = jumpTimer;
+        startTimer = false;
     }
 
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
         //Debug.Log(raycastHit2D.collider);
+        Debug.DrawRay(boxCollider2D.bounds.center, (boxCollider2D.bounds.size * Vector2.down), Color.red);
 
         return raycastHit2D.collider != null;
     }
