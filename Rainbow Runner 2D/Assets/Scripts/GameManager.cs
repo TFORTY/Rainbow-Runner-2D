aee6e0ffcf903ebdfd79e2803d2fc3fe5ceafd18
Tyler Wong <tywong8@gmail.com>
2023-08-12 17:52:17 -0400

    parallax background effect

--- Rainbow Runner 2D/Assets/Scripts/GameManager.cs
+++ Rainbow Runner 2D/Assets/Scripts/GameManager.cs
@@ -13,7 +13,25 @@ public class GameManager : MonoBehaviour
     // Deal with game starting, losing, restarting
     // Audio Sources, Game Music, Game Ambience
 
-    // Singleton
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] float cameraSpeed;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Transform cameraTransform;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +42,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerTransform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void LateUpdate()
+    {
+        cameraTransform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
     }
 }
