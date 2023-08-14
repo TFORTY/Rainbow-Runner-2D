d907bdab5577f404584a879d079578b1085fb923
Tyler Wong <tywong8@gmail.com>
2023-08-14 16:46:14 -0400

    basic infinite platform generator

--- Rainbow Runner 2D/Assets/Scripts/LevelGenerator.cs
+++ Rainbow Runner 2D/Assets/Scripts/LevelGenerator.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200f;
+
+    [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform levelPart_1;
+
+    private Vector3 lastEndPosition;
 
     private void Awake()
     {
-        SpawnLevelPart(new Vector3(36, -11));
-        SpawnLevelPart(new Vector3(36, -11) + new Vector3(20, 0));
-        SpawnLevelPart(new Vector3(36, -11) + new Vector3(20 + 20, 0));
+        lastEndPosition = levelPart_Start.Find("EndPosition").position;
+
+        int startingSpawnLevelParts = 5;
+
+        for (int i = 0; i < startingSpawnLevelParts; ++i)
+        {
+            SpawnLevelPart();
+        }
     }
 
     // Start is called before the first frame update
@@ -22,11 +32,22 @@ public class LevelGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(Player.Instance.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+        {
+            // Spawn another level part
+            SpawnLevelPart();
+        }
     }
 
-    private void SpawnLevelPart(Vector3 spawnPosition)
+    private void SpawnLevelPart()
     {
-        Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
